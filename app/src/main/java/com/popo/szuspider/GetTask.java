2599e07b74a9b28dca50ce81da538c4aa93db176
noterpopo <popo54336@outlook.com>
2018-04-10 15:01:24 +0800

    Initial commit

---
+++ app/src/main/java/com/popo/szuspider/GetTask.java
@@ -0,0 +1,20 @@
+package com.popo.szuspider;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by lgp on 2018/4/10.
+ */
+
+public class GetTask extends AsyncTask<Void,Void,Void>{
+    @Override
+    protected Void doInBackground(Void... voids) {
+        
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+    }
+}
